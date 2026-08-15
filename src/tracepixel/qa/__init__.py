0fd6c9409892f7d7d6f91e5c54c776143a1bbc8d
khsfashi <55976865+khsfashi@users.noreply.github.com>
2026-08-15 22:59:12 +0900

    Add P4-Q0 QA package surface

---
+++ src/tracepixel/qa/__init__.py
@@ -0,0 +1,23 @@
+"""Deterministic pixel-QA fact surfaces."""
+
+from tracepixel.qa.structural import (
+    STRUCTURAL_QA_SCHEMA_V1,
+    AlphaFactsV1,
+    BoundsV1,
+    DimensionsV1,
+    EdgeContactV1,
+    MarginsV1,
+    StructuralFactsV1,
+    analyze_structural,
+)
+
+__all__ = [
+    "STRUCTURAL_QA_SCHEMA_V1",
+    "AlphaFactsV1",
+    "BoundsV1",
+    "DimensionsV1",
+    "EdgeContactV1",
+    "MarginsV1",
+    "StructuralFactsV1",
+    "analyze_structural",
+]
