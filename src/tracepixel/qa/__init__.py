d3b24899994a7703060bf9e5f6b04c3ec4ec3bc8
khsfashi <55976865+khsfashi@users.noreply.github.com>
2026-08-15 23:15:03 +0900

    Expose P4-Q1 color QA API

--- src/tracepixel/qa/__init__.py
+++ src/tracepixel/qa/__init__.py
@@ -1,5 +1,18 @@
 """Deterministic pixel-QA fact surfaces."""
 
+from tracepixel.qa.color import (
+    COLOR_QA_SCHEMA_V1,
+    MAX_COLOR_POLICY_COLORS_V1,
+    ColorCountFactsV1,
+    ColorQaConfigurationError,
+    ColorQaV1,
+    MaximumColorCheckV1,
+    PaletteMembershipCheckV1,
+    TransparentRgbCheckV1,
+    TransparentRgbFactsV1,
+    TransparentRgbPolicyV1,
+    analyze_color,
+)
 from tracepixel.qa.structural import (
     STRUCTURAL_QA_SCHEMA_V1,
     AlphaFactsV1,
@@ -12,6 +25,17 @@ from tracepixel.qa.structural import (
 )
 
 __all__ = [
+    "COLOR_QA_SCHEMA_V1",
+    "MAX_COLOR_POLICY_COLORS_V1",
+    "ColorCountFactsV1",
+    "ColorQaConfigurationError",
+    "ColorQaV1",
+    "MaximumColorCheckV1",
+    "PaletteMembershipCheckV1",
+    "TransparentRgbCheckV1",
+    "TransparentRgbFactsV1",
+    "TransparentRgbPolicyV1",
+    "analyze_color",
     "STRUCTURAL_QA_SCHEMA_V1",
     "AlphaFactsV1",
     "BoundsV1",
