7be358386051d9b830f817d9235953f32f08922f
khsfashi <55976865+khsfashi@users.noreply.github.com>
2026-08-16 12:02:35 +0900

    Export P6-V1 contact sheet API

--- src/tracepixel/preview/__init__.py
+++ src/tracepixel/preview/__init__.py
@@ -11,15 +11,27 @@ from tracepixel.preview.bundle import (
     build_preview_bundle,
     write_preview_bundle,
 )
+from tracepixel.preview.contact_sheet import (
+    STAGE_CONTACT_SHEET_SCHEMA_V1,
+    StageContactSheet,
+    StageContactSheetContractError,
+    build_stage_contact_sheet,
+    write_stage_contact_sheet,
+)
 
 __all__ = [
     "PREVIEW_BUNDLE_SCHEMA_V1",
+    "STAGE_CONTACT_SHEET_SCHEMA_V1",
     "PreviewBundle",
     "PreviewBundleContractError",
     "PreviewBundleFile",
     "PreviewStageArtifact",
     "StageArtifactKindV1",
     "StageArtifactMediaTypeV1",
+    "StageContactSheet",
+    "StageContactSheetContractError",
     "build_preview_bundle",
+    "build_stage_contact_sheet",
     "write_preview_bundle",
+    "write_stage_contact_sheet",
 ]
